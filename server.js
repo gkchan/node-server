c9b530d619296384c77aadfc42dc922bf5e554b4
Grace <gkchan09@gmail.com>
2018-01-07 11:33:49 +0000

    Start basic node express server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello world')
+    console.log("test1")
+})
+
+app.listen(5001)
+
+// app.listen(5001, () => console.log("test2"))
+
+
+// var server = app.listen(3000, function () {
+//     var host = server.address().address
+//     var port = server.address().port
+
+//     console.log("test3")
+// })
+
+// set port
